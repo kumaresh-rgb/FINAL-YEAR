b7cd3b56abb5f3621215059bf3a8df8e9fe1f486
Anurag Maurya <mauryaanurag375@gmail.com>
2023-03-02 07:24:55 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { BrowserRouter,Link, Route,Routes} from 'react-router-dom';
+import {logo} from "./assets";
+import {Home,CreatePost} from "./pages";
+const App = () => {
+  return (
+    <BrowserRouter>
+      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+      {/* w-full is window full width ,flex makes it a flex container, justify between is justfying space inbetween , items-center jsutifiy items 
+      center, px-8 -> padding left and right 1rem ,border-b	-> border-bottom-width: 1px; #e6ebf4 is color set to border */}
+      <Link to="/">
+        <img src={logo} alt="logo" className="w-28 object-contain"/>
+      </Link>
+      <Link to="/create-post" className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>Create</Link>
+      </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
+      </main>
+      <footer className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4] place-content-end '>
+      <a href='https://github.com/Anuragmaurya-code'><p className='text-right hover:text-[#BDCDD6] '>Made by Anurag Maurya</p></a>
+      
+    </footer>
+    </BrowserRouter>
+    
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from  "cors";
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";// importing routes
+import dalleRoutes from "./routes/dalleRoutes.js";
+
+
+dotenv.config();
+
+const app=express();
+app.use(cors());
+app.use(express.json({limit:'50mb'})); // enables the server to parse incoming requests with JSON payloads up to a limit of 50MB.
+//data being send cannot exceed 50MB
+app.use('/api/v1/post',postRoutes);// any HTTP requests that match the route path /api/v1/post will be handled by the middleware 
+//functions defined in the postRoutes module.
+app.use('/api/v1/dalle',dalleRoutes);
+app.get("/",async(req, res)=>{
+    res.send('Hello from DALLE');
+});
+
+const startServer = async()=>{
+    try{
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080,()=>{
+            console.log('server has started on port http://localhost:8080');
+        })
+    }catch(error){
+        console.log(error);
+    }
+;}
+startServer();
