8bcc21583af142bacf895e574ea0a8f897a13821
Anurag Maurya <mauryaanurag375@gmail.com>
2023-03-02 20:35:43 +0530

    H

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://openai-kswb.onrender.com/api/v1/post', {
           method: 'GET',
           header: {
             'Content-Type': 'application/json',
